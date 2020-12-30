6afb958ba2c71984a9a6c74840ad92ad45285c30
Michal Kacprzak <michal.kacprzak999@gmail.com>
2020-12-30 17:34:03 +0100

    change layout

--- frontend/src/pages/AdminPage/components/AdminInterface/AdminInterface.js
+++ frontend/src/pages/AdminPage/components/AdminInterface/AdminInterface.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import { Layout, UserNickname, CreepyHello, OptionArea, Navbar} from './AdminInterface.css';
-import {Adder, UsersController} from './components'
+import { OptionArea, Navbar} from './AdminInterface.css';
+import {Adder, UsersController, UsersMessages} from './components'
 const AdminInterface = () => {
     const [adminActivity, setAdminActivity] = useState(null)
     const clickHandler = (e) => {
@@ -12,26 +12,22 @@ const AdminInterface = () => {
         if(id === 7){
             setAdminActivity(<UsersController/>)
         }
+        if(id === 6){
+            setAdminActivity(<UsersMessages/>)
+        }
     }
     return (
         <>
-            <Layout>
-                <UserNickname>Admin</UserNickname>
-                <Navbar>  
-                <button id='1' name="accept recipe" onClick={clickHandler}>Akceptuj Przepis</button>             
-                <button id='2' name="product" onClick={clickHandler}>Dodaj produkt</button>
-                <button id='3' name="product category" onClick={clickHandler}>Dodaj kategorie produktu</button>
-                <button id='4' name="recipe category" onClick={clickHandler}>Dodaj kategorie przepisu</button>
-                <button id='5' name="forum topic" onClick={clickHandler}>Dodaj temat na forum</button>
-                <button id='6' name="user message" onClick={clickHandler}>Wiadomości od użytkowników</button>
-                <button id='7' name="control users" onClick={clickHandler}>Zablokuj/Odblokuj uzytkowników</button>
-
-                </Navbar>
-                <CreepyHello>Witaj Drogi Adminie</CreepyHello>
-                <OptionArea>
-                   {adminActivity}
-                </OptionArea>
-            </Layout>
+            <Navbar>  
+            <button id='1' name="accept recipe" onClick={clickHandler}>Akceptuj Przepis</button>             
+            <button id='2' name="product" onClick={clickHandler}>Dodaj produkt</button>
+            <button id='3' name="product category" onClick={clickHandler}>Dodaj kategorie produktu</button>
+            <button id='4' name="recipe category" onClick={clickHandler}>Dodaj kategorie przepisu</button>
+            <button id='5' name="forum topic" onClick={clickHandler}>Dodaj temat na forum</button>
+            <button id='6' name="user message" onClick={clickHandler}>Wiadomości od użytkowników</button>
+            <button id='7' name="control users" onClick={clickHandler}>Zablokuj/Odblokuj uzytkowników</button>
+            </Navbar>
+            {adminActivity}
         </> 
         )
 }
