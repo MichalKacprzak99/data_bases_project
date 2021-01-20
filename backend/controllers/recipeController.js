685af27e9cbef2532c0be5afead3b9c68df48fbb
Michal Kacprzak <michal.kacprzak999@gmail.com>
2021-01-20 10:43:29 +0100

    add error handling for adding recipes, fix getting recipes/recipe -> for admin purpose, include views in queries, switch response lang from en to pl, add option to reject recipe and also filter recipes

--- backend/controllers/recipeController.js
+++ backend/controllers/recipeController.js
@@ -17,10 +17,20 @@ const addRecipe = async(request, response) => {
     const categories = JSON.stringify(request.body.categories);
     const name = request.body.name;
     const description = request.body.description;
-  
     pool.query(`SELECT * FROM dodaj_przepis($1,NOW()::timestamp,$2, $3, $4, $5)`,[user_id, name, description, products, categories], async(error, results) => {
-      if (error) {
-        return response.status(409).json({ status: 'failed', message: error.stack });
+      if(error){
+
+        let errorMessage
+        if (error.code == 'P0001') {
+
+          errorMessage = error.message
+
+        }
+        else {
+
+          errorMessage = 'Nie możesz dodać dwa razy jednego produktu'
+        }
+        return response.status(409).json({ status: 'failed', message: errorMessage });
       } else {
         return response.status(200).json(true);
       }
@@ -34,92 +44,122 @@ const addRecipe = async(request, response) => {
 
 const getRecipes = async(request, response) => {
   const token = request.body.token;
-  // const id_recipe = request.query.id_recipe
+  const adminToken = request.body.adminToken;
   const status = request.body.status;
-  try {
+  const onlyLiked = request.body.onlyLiked;
+
+  if(onlyLiked){
     const decode = jwt.verify(token, PrivateKey);
     const user_id = decode._id;
     
-    const query =`SELECT *, exists(select * from polubione_przepisy WHERE "id_przepis"=przepis.id_przepis AND "id_uzytkownika"=$2) as polubiony FROM przepis 
-      JOIN akceptacja_przepisu USING(id_przepis)
-      WHERE status=$1`
+    const query =`SELECT * FROM recipes JOIN Polubione_przepisy USING (id_przepis)
+      WHERE status=$1 and Polubione_przepisy.id_likujacego=$2`
     pool.query(query,[status, user_id], async(error, results) => {
-      if (error) {
-        console.log(erros.stack)
-        return response.status(409).json({ status: 'failed', message: error.stack });
-      } else {
-        return response.status(200).json(results.rows);
-      }
-    });
-  } catch(err) {
-    const query = `SELECT *, false as polubiony FROM przepis 
-    JOIN akceptacja_przepisu USING(id_przepis)
-    WHERE status=$1`
-    pool.query(query,[status], async(error, results) => {
-      if (error) {
-        return response.status(409).json({ status: 'failed', message: error.stack });
-      } else {
-        return response.status(200).json(results.rows);
-      }
-  });
-  }
-
-
-  
-
-};
-
-const getRecipe = async(request, response) => {
-  const token = request.body.token;
-  const id_recipe = request.query.id_recipe
-  try{
-    const decode = jwt.verify(token, PrivateKey);
-    const user_id = decode._id;
-    const query = `SELECT nazwa, opis, data_dodania, exists(select * from polubione_przepisy WHERE "id_przepis"=$1 AND "id_uzytkownika"=$2) as polubiony
-  
-      FROM przepis
-      JOIN akceptacja_przepisu USING (id_przepis)
-      WHERE id_przepis=$1 AND status='zaakceptowany'`
-    pool.query(query,[id_recipe, user_id], async(error, results) => {
       if (error) {
         console.log(error.stack)
         return response.status(409).json({ status: 'failed', message: error.stack });
       } else {
-
-        if(results.rows.length !== 0){
-
-          return response.status(200).json(results.rows[0]);
-        } else {
-          return response.status(200).json(null);
-        }
-        
-      }
-  });
-} catch(err) {
-    const query = `SELECT nazwa, opis, data_dodania, false as polubiony FROM przepis 
-    WHERE id_przepis=$1`
-    pool.query(query,[id_recipe], async(error, results) => {
-      if (error) {
-        return response.status(409).json({ status: 'failed', message: error.stack });
-      } else {
-        return response.status(200).json(results.rows[0]);
+        return response.status(200).json(results.rows);
       }
     });
-  }  
+  } else {
+        let query
+        if(status =="oczekujący") {
+            const decode = jwt.verify(adminToken, PrivateKey);
+            const admin_id = decode._id;
+            query = `SELECT * FROM recipes WHERE status=$1 and id_admin=${admin_id}`
+        } else {
+            query = `SELECT * FROM recipes WHERE status=$1`
+        }
+        
+        pool.query(query,[status], async(error, results) => {
+            if (error) {
+                return response.status(409).json({ status: 'failed', message: error.stack });
+            } else {
+                return response.status(200).json(results.rows);
+            }
+        });
+  }
+
+
+};
+
+const getRecipe = async(request, response) => {
+  
+  const token = request.body.token;
+
+  const id_recipe = request.query.id_recipe
+  const status = request.query.status
+
+  if(status=="oczekujący"){
+    const adminToken = request.body.adminToken;
+    try{
+      const decode = jwt.verify(adminToken, PrivateKey);
+      const admin_id = decode._id;
+
+      const query = `SELECT nazwa, opis, data_dodania, false as polubiony FROM recipes
+      WHERE id_przepis=$1 AND status=$2`
+  
+      pool.query(query,[id_recipe,status], async(error, results) => {
+        if (error) {
+          console.log(error.stack)
+          return response.status(409).json({ status: 'failed', message: error.stack });
+        } else {
+          return response.status(200).json(results.rows[0]);
+        }
+      });
+    }
+    catch {
+      return response.status(200).json(null);
+    }
+  }else {
+    try{
+      const decode = jwt.verify(token, PrivateKey);
+      const user_id = decode._id;
+      const query = `SELECT *, EXISTS(SELECT * FROM polubione_przepisy WHERE "id_przepis"=$1 AND "id_likujacego"=$2) as polubiony FROM
+        recipes WHERE id_przepis=$1 AND status=$3`
+
+      pool.query(query,[id_recipe, user_id,status], async(error, results) => {
+        if (error) {
+          console.log(error.stack)
+          return response.status(409).json({ status: 'failed', message: error.stack });
+        } else {
+  
+          if(results.rows.length !== 0){
+  
+            return response.status(200).json(results.rows[0]);
+          } else {
+            return response.status(200).json(null);
+          }
+          
+        }
+    });
+  } catch(err) {
+      const query = `SELECT nazwa, opis, data_dodania, false as polubiony FROM przepis 
+      WHERE id_przepis=$1 AND status=$2`
+  
+      pool.query(query,[id_recipe,status], async(error, results) => {
+        if (error) {
+          return response.status(409).json({ status: 'failed', message: error.stack });
+        } else {
+          return response.status(200).json(results.rows[0]);
+        }
+      });
+    }  
+  }
+  
 
 };
 
 const getRecipeProducts = async(request, response) => {
+  
   const id_recipe = request.query.id_recipe
 
-  const query =  `SELECT *, kategoria_produkt.nazwa as kategoria, produkt.nazwa FROM przepis_produkt
-  JOIN produkt USING (id_produkt)
-  JOIN kategoria_produkt ON produkt.id_kategoria_produkt = kategoria_produkt.id_kategoria_produkt
-  WHERE id_przepis=$1`
+  const query =  `SELECT * FROM recipe_products WHERE id_przepis=$1`
   
   pool.query(query,[id_recipe], async(error, results) => {
     if (error) {
-      console.log(error.stack)
+      console.log(error)
       return response.status(409).json({ status: 'failed', message: error.stack });
     } else {
       return response.status(200).json(results.rows);
@@ -129,12 +169,9 @@ const getRecipeProducts = async(request, response) => {
 };
 
 const getRecipeCategories = async(request, response) => {
+  
   const id_recipe = request.query.id_recipe
-
-  const query =  `SELECT kategoria_przepis.nazwa FROM kategoria_przepis 
-  JOIN asocjacja_kategoria_przepis USING (id_kategoria_przepis)
-  WHERE "id_przepis"=$1;
-  `
+  const query =  `SELECT nazwa FROM recipe_category WHERE "id_przepis"=$1;`
   pool.query(query,[id_recipe], async(error, results) => {
     if (error) {
       console.log(error.stack)
@@ -154,8 +191,7 @@ const likeRecipe = async(request, response) => {
   const decode = jwt.verify(token, PrivateKey);
   const user_id = decode._id;
 
-  const query =  `INSERT INTO polubione_przepisy VALUES ($1,$2,$3,to_timestamp($4));
-  `
+  const query =  `INSERT INTO polubione_przepisy SELECT $1, id_uzytkownika, $2,  to_timestamp($4), $3 from recipes WHERE id_przepis = $1 `
   pool.query(query,[id_recipe,user_id ,description, Date.now()/1000,], async(error, results) => {
     if (error) {
       console.log(error.stack)
@@ -173,9 +209,7 @@ const dislikeRecipe = async(request, response) => {
   const token = request.body.token;
   const decode = jwt.verify(token, PrivateKey);
   const user_id = decode._id;
-  const query =  `DELETE FROM  polubione_przepisy
-  WHERE id_przepis=$1 and id_uzytkownika=$2;
-  `
+  const query =  `DELETE FROM  polubione_przepisy WHERE id_przepis=$1 and id_likujacego=$2;`
   pool.query(query,[id_recipe, user_id], async(error, results) => {
     if (error) {
       console.log(error.stack)
@@ -194,9 +228,10 @@ const addComment = async(request, response) => {
   const token = request.body.token;
   const decode = jwt.verify(token, PrivateKey);
   const user_id = decode._id;
-
-  const query =  `INSERT INTO komentarz (id_uzytkownika, id_przepis, tresc, data_dodania) VALUES ($1,$2,$3,to_timestamp($4));`
-  pool.query(query,[user_id, id_recipe ,description, Date.now()/1000,], async(error, results) => {
+  // id_komentujacego
+  const query =  `INSERT INTO komentarz(id_uzytkownika,id_przepis,id_komentujacego,tresc,data_dodania) SELECT id_uzytkownika,$1, $2, $3,  to_timestamp($4) from recipes WHERE id_przepis = $1 `
+  // const query =  `INSERT INTO komentarz (id_uzytkownika, id_przepis, tresc, data_dodania) VALUES ($1,$2,$3,to_timestamp($4));`
+  pool.query(query,[id_recipe, user_id ,description, Date.now()/1000,], async(error, results) => {
     if (error) {
       console.log(error.stack)
       return response.status(409).json({ status: 'failed', message: error.stack });
@@ -207,9 +242,9 @@ const addComment = async(request, response) => {
 
 };
 const getComments = async(request, response) => {
+  
   const id_recipe = request.query.id_recipe
-  const query =  `SELECT id_komentarz, uzytkownik.pseudonim, tresc FROM komentarz JOIN uzytkownik USING (id_uzytkownika) 
-  WHERE id_przepis=$1`
+  const query =  `SELECT * FROM comments WHERE id_przepis=$1`
   pool.query(query,[id_recipe], async(error, results) => {
     if (error) {
       console.log(error.stack)
@@ -236,6 +271,82 @@ const acceptRecipe = async(request, response) => {
 
 };
 
+const rejectRecipe = async(request, response) => {
+  const id_recipe = request.query.id_recipe
+  const query =  `UPDATE akceptacja_przepisu SET status='odrzucony' WHERE id_przepis=$1  ;`
+  pool.query(query,[id_recipe], async(error, results) => {
+    if (error) {
+      console.log(error.stack)
+      return response.status(409).json({ status: 'failed', message: error.stack });
+    } else {
+      return response.status(200).json(true);
+    }
+  });
+
+};
+
+
+const filterRecipes = async(request, response) => {
+    const token = request.body.token;
+    const status = request.body.status;
+    const date = request.body.added_date
+    const name= request.body.name
+    const minimumProductNumber= request.body.minimumProductNumber
+    const categories= request.body.categories
+    const productCategories= request.body.productCategories
+    const onlyLiked = request.body.onlyLiked;
+    console.log(onlyLiked)
+    let query
+    if(onlyLiked){
+
+        const decode = jwt.verify(token, PrivateKey);
+        const user_id = decode._id;
+
+        query = `SELECT id_przepis,recipes.nazwa, opis, recipes.data_dodania, notatka FROM recipes
+        JOIN asocjacja_kategoria_przepis USING(id_przepis)
+        JOIN kategoria_przepis USING(id_kategoria_przepis)
+        JOIN przepis_produkt USING(id_przepis)
+        JOIN produkt USING(id_produkt)
+        JOIN Polubione_przepisy USING (id_przepis)
+        JOIN kategoria_produkt ON produkt.id_kategoria_produkt = kategoria_produkt.id_kategoria_produkt
+        WHERE status=$1 AND kategoria_przepis.nazwa IN (${categories}) AND kategoria_produkt.nazwa in (${productCategories}) AND Polubione_przepisy.id_likujacego=$2
+        GROUP BY id_przepis, opis, recipes.data_dodania,recipes.nazwa, notatka
+        HAVING ${parseInt(minimumProductNumber)} <= (SELECT COUNT(*) FROM przepis_produkt WHERE id_przepis = recipes.id_przepis)
+        ORDER BY  recipes.data_dodania::date ${date}, recipes.nazwa ${name}
+        `
+        pool.query(query,[status, user_id], async(error, results) => {
+        if (error) {
+            console.log(error)
+            return response.status(409).json({ status: 'failed', message: error.stack });
+        } else {
+            console.log(results.rows)
+            return response.status(200).json(results.rows);
+        }
+        });
+    } else {
+        query = `SELECT id_przepis,recipes.nazwa, opis, data_dodania FROM recipes
+        JOIN asocjacja_kategoria_przepis USING(id_przepis)
+        JOIN kategoria_przepis USING(id_kategoria_przepis)
+        JOIN przepis_produkt USING(id_przepis)
+        JOIN produkt USING(id_produkt)
+        JOIN kategoria_produkt ON produkt.id_kategoria_produkt = kategoria_produkt.id_kategoria_produkt
+        WHERE status=$1 AND kategoria_przepis.nazwa IN (${categories}) AND kategoria_produkt.nazwa in (${productCategories})
+        GROUP BY id_przepis, opis, data_dodania,recipes.nazwa
+        HAVING ${parseInt(minimumProductNumber)} <= (SELECT COUNT(*) FROM przepis_produkt WHERE id_przepis = recipes.id_przepis)
+        ORDER BY  data_dodania::date ${date}, recipes.nazwa ${name}
+        `
+        pool.query(query,[status], async(error, results) => {
+            if (error) {
+                console.log(error.stack)
+                return response.status(409).json({ status: 'failed', message: error.stack });
+            } else {
+                return response.status(200).json(results.rows);
+            }
+        });
+    }
+
+}
+
 export default {
     addRecipe,
     acceptRecipe,
@@ -247,4 +358,6 @@ export default {
     getRecipeProducts,
     getRecipes,
     getRecipe,
+    filterRecipes,
+    rejectRecipe,
 }
