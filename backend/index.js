c1757a10e9d6d0127583677b8ef5a00e6036766a
Michal Kacprzak <michal.kacprzak999@gmail.com>
2021-02-20 15:37:52 +0100

    change title of website

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,12 @@ import cookieParser from "cookie-parser";
 import bodyParser from 'body-parser';
 import swaggerJSDoc from "swagger-jsdoc"
 import swaggerUi from "swagger-ui-express"
+
+import recipeRouter from './routes/recipeRouter.js';
+import adminRouter from './routes/adminRouter.js';
+import userRouter from './routes/userRouter.js';
+import forumRouter from './routes/forumRouter.js';
+
 dotenv.config();
 
 
@@ -20,14 +26,13 @@ const swaggerDefinition = {
 
 const options = {
   swaggerDefinition,
-  // Paths to files containing OpenAPI definitions
   apis: ['./routes/*.js'],
 };
 
 const swaggerSpec = swaggerJSDoc(options);
 
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.DB_PORT || 8000;
 
 app.use(cookieParser());
 app.use(cors())
@@ -36,10 +41,7 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json({extended: true}));
 app.use(bodyParser.json());
 
-import recipeRouter from './routes/recipeRouter.js';
-import adminRouter from './routes/adminRouter.js';
-import userRouter from './routes/userRouter.js';
-import forumRouter from './routes/forumRouter.js';
+
 
 app.use('/recipes', recipeRouter)
 app.use('/admin', adminRouter)
